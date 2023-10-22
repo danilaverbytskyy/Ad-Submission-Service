e0e59151f564017569ba3e53f516ca148f478250
Danila <dod.shreder@gmail.com>
2023-10-22 21:43:17 +0300

    added cards on home page

--- app/views/home.php
+++ app/views/home.php
@@ -1,8 +1,55 @@
 <?php $this->layout('layout') ?>
 
 <section id="banner">
-    <div class="container-fluid bg-success bg-gradient h-auto">
+    <div class="container-fluid bg-success bg-gradient h-auto" style="padding-left: 10%;">
         <h1 class="display-4 pt-5">Мы - Лучший Сервис Для Ваших Объявлений!</h1>
-        <p class="lead pb-5">Настроение и вдохновение в каждом товаре.</p></div>
+        <p class="lead pb-5">Настроение и вдохновение в каждом товаре.</p>
+    </div>
+</section>
+
+<section id="advertisments">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-3 mb-3 mb-sm-3">
+                <div class="card" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3 mb-3 mb-sm-3">
+                <div class="card" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3 mb-3 mb-sm-3">
+                <div class="card" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3 mb-3 mb-sm-3">
+                <div class="card" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
